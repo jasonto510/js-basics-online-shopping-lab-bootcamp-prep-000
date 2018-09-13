b21bf930d6a4ca98ea2b5c28e8a9248918baeb4a
Jason To <jasto@ucdavis.edu>
2018-09-13 03:56:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,19 +10,39 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 99) + 1;
+  cart.push({itemName: item,
+            itemPrice: price 
+  });
+  return (`${item} has been added to your cart.`);
 }
 
+
+
 function viewCart() {
-  // write your code here
+  if (cart.length === 0){
+    return ("Your shopping cart is empty");
+  } else{
+    var MyCart = (`In your cart, you have `);
+  for (var i = 0; i < cart.length ; i ++){
+  }
+  }
+  
 }
 
 function total() {
-  // write your code here
+  var t = 0;
+  for (var i = 0; i < cart.length; i ++){
+  t += cart[i].itemPrice; 
+  }
+return t;
 }
 
 function removeFromCart(item) {
   // write your code here
+  for (var i = 0; i < cart.length; i++){
+    if item 
+  }
 }
 
 function placeOrder(cardNumber) {
