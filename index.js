2fdc9dfb1da154e95f28b2f84e4988b9039d906c
Jason To <jasto@ucdavis.edu>
2018-09-13 05:07:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function placeOrder(cardNumber) {
    if (cardNumber === undefined){
      return ("Sorry, we don't have a credit card on file for you.");
    }else{
-     var value = (`Your total cost is $${total(cart2)}, which will be charged to the card ${cardNumber}.`);
+     var value = (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
     cart = [];
      return value;
 }
