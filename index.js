50f64a6d69fab6f5da34f17d6df2c8d09a5cee6c
Jason To <jasto@ucdavis.edu>
2018-09-13 04:11:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++){
     if (item == cart[i]){
       delete cart[i];
-      return cart;
+      return cart[i];
     }else{
       return ("That item is not in your cart.");
     }
