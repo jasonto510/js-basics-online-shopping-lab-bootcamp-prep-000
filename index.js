e743220a4501aa15418f33e37d347b80eb5ea1d7
Jason To <jasto@ucdavis.edu>
2018-09-13 04:23:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,8 +51,11 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
-   if (cart === undefined){
-     return
-   }
-  
+   if (cardNumber === undefined){
+     return ("Sorry, we don't have a credit card on file for you.");
+   }else{
+     empty_cart = [];
+     cart = empty_cart;
+     return (`Your total cost is $${total}, which will be charged to your ${cardNumber}`);
+}
 }
