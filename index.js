3f33976f5b7442c238ed5cddc4ee14d422718dbf
Jason To <jasto@ucdavis.edu>
2018-09-13 05:29:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,12 +43,13 @@ function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++){
     if (item === cart[i]){
       repeat = item;
+      cart.splice(i, 1);
     }
   }
   if (repeat.length === 0){
     return("That item is not in your cart.");
   }else{
-    return cart.splice(i, 1);
+    return cart;
   } 
 }
 
